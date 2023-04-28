dfb4f840ed9c8446c9832b31f020889d9de76553
Daniela Cardozo <mdanielacardozo@gmail.com>
2023-04-28 15:23:32 -0300

    Agregué carpetas

--- src/components/views/QuienesSomos.js
+++ src/components/views/QuienesSomos.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const Menu = () => {
+const QuienesSomos = () => {
     return (
         <div>
             
@@ -8,4 +8,4 @@ const Menu = () => {
     );
 };
 
-export default Menu;
+export default QuienesSomos;
