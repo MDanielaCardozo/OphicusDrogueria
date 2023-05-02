9539fbc2afc214936ba6ce4af91d2f2fab844e04
Daniela Cardozo <mdanielacardozo@gmail.com>
2023-05-02 15:16:52 -0300

    Agregué algo del home

--- src/components/views/Home.js
+++ src/components/views/Home.js
@@ -1,15 +1,60 @@
-import React from 'react';
+import React from "react";
 import "../views/Home.css";
+import { Col, Row } from "react-bootstrap";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBoxesPacking, faTruckFast, faTruckRampBox } from "@fortawesome/free-solid-svg-icons";
 
 const Home = () => {
-    return (
-        <div>
-            <section className='portada'>
-
-            </section>
-            
+  return (
+    <div>
+      <section className="portada text-light">
+        {/* <h1 className=''>Ophicus</h1> */}
+      </section>
+      <section className="servicios py-5">
+        <div className="container text-center">
+          <h2>Nuestros Servicios</h2>
+          <Row className="w-100">
+            <Col sm={12} md={4} lg={4} >
+              <div className="cardInfo w-100 p-2">
+                <div className="icono">
+                  <FontAwesomeIcon icon={faBoxesPacking} className="icon"/>
+                </div>
+                <h3>Trazabilidad</h3>
+                <p className="m-3">
+                  Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                  Soluta, id! Voluptates minus.
+                </p>
+              </div>
+            </Col>
+            <Col sm={12} md={4} lg={4}>
+            <div className="cardInfo w-100 p-2">
+                <div className="icono">
+                  <FontAwesomeIcon icon={faTruckFast} className="icon"/>
+                </div>
+                <h3>Logística</h3>
+                <p className="m-3">
+                  Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                  Soluta, id! Voluptates minus.
+                </p>
+              </div>
+            </Col>
+            <Col sm={12} md={4} lg={4}>
+            <div className="cardInfo w-100 p-2">
+                <div className="icono">
+                  <FontAwesomeIcon icon={faTruckRampBox} className="icon"/>
+                </div>
+                <h3>Proveedores</h3>
+                <p className="m-3">
+                  Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                  Soluta, id! Voluptates minus.
+                </p>
+              </div>
+            </Col>
+          </Row>
         </div>
-    );
+      </section>
+    </div>
+  );
 };
 
 export default Home;
