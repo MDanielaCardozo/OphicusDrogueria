58a7b555a4265398c70694965b601273e4894a3b
Daniela Cardozo <mdanielacardozo@gmail.com>
2023-05-03 10:05:52 -0300

    Agregué contacto y form para el mail

--- src/components/views/Contacto.js
+++ src/components/views/Contacto.js
@@ -1,9 +1,47 @@
 import React from 'react';
+import "../views/Contacto.css";
+import { Col, Form, Row, Button } from 'react-bootstrap';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEnvelope, faLocationDot, faPhone } from '@fortawesome/free-solid-svg-icons';
 
 const Contacto = () => {
     return (
-        <div>
-            
+        <div className='contacto'>
+            <section className='container text-center'>
+                <h2>Contacto</h2>
+                <hr />
+            </section>
+            <section className='container'>
+                <Row>
+                    <Col lg={6} md={6} sm={12} className='my-4'>
+                    <div className='d-flex'>
+                        <FontAwesomeIcon icon={faLocationDot} className='iconoContacto mx-4'/>
+                        <p>Ayacucho 830, San Miguel de Tucumán. Argentina.</p>
+                    </div>
+                    <div className='d-flex'>
+                        <FontAwesomeIcon icon={faEnvelope} className='iconoContacto mx-4'/>
+                        <p>drogueriaophicus@gmail.com</p>
+                    </div>
+                    <div className='d-flex'>
+                        <FontAwesomeIcon icon={faPhone} className='iconoContacto mx-4'/>
+                        <p>381-6833503</p>
+                    </div>
+                    </Col>
+                    <Col lg={6} md={6} sm={12}>
+                        <div className='contactoBox'>
+                            <Form.Group className='my-4'>
+                                <Form.Label>Nombre Completo*</Form.Label>
+                                <Form.Control type='text' placeholder='Nombre Apellido'/>
+                                <Form.Label>Email*</Form.Label>
+                                <Form.Control type='text' placeholder='Ej: pilargarcia@gmail.com'/>
+                                <Form.Label>Mensaje*</Form.Label>
+                                <Form.Control as='textarea' rows={4}/>
+                                <Button variant='outline-light' className='my-3 ms-1'>Enviar</Button>
+                            </Form.Group>
+                        </div>
+                    </Col>
+                </Row>
+            </section>
         </div>
     );
 };
