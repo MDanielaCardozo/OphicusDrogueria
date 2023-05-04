ca7bab727c34e33779ee544cc4a46758ef62d13c
Daniela Cardozo <mdanielacardozo@gmail.com>
2023-05-04 00:46:40 -0300

    Agregué portada la Pag servicios

--- src/components/views/Servicios.js
+++ src/components/views/Servicios.js
@@ -3,6 +3,13 @@ import React from 'react';
 const Servicios = () => {
     return (
         <div>
+            <section className='portadaServicios'></section>
+            <article className='quienes py-5'>
+                <div className='container text-center'>
+                    <h2>Servicios</h2>
+                    <hr />
+                </div>
+            </article>
             
         </div>
     );
